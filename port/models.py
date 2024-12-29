79440243e94b1d1fa234ae38f4ce30d4fe09a183
shajanjjacob <shajanjjacob369@gmail.com>
2024-12-30 02:46:09 +0530

    Initial commit

---
+++ port/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.db import models
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+# Create your models here.
---
+++ staticfiles/js/about.js
@@ -0,0 +1,5 @@
+AOS.init({
+    duration: 1000, // Animation duration in milliseconds
+    easing: 'ease-in-out', // Easing type
+    once: true, // Whether animation should happen only once
+});
